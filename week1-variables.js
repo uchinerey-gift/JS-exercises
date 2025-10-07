a2ee5e45f410cef0424306d4af330f6bb4c36d2e
Chinerey <uchinerey@gmail.com>
2025-10-06 22:32:53 -0700

    add week 1 variables exercises

---
+++ week1-variables.js
@@ -0,0 +1,39 @@
+// Week 1 - Variables
+
+// Exercise 1. Simply declare a variable named 'emptyVariable'
+let emptyVariable;
+
+// Exercise 2. Declare a variable called 'myName'
+let myName;
+
+// Exercise 3. Assign your name as the value for 'myName'
+myName = "Chinerey";
+
+// Exercise 4. Declare a variable called dreamDestination and give it the value
+// of what your dream vacation destination is.
+let dreamDestination = "Bali";
+
+// Exercise 5. Make the following variables and assignments
+// - a variable called num1 with a value greater than 1 and less than 10
+// - a variable called num2 with a value of greater than or equal to 1 and less
+//   than or equal to 10
+let num1 = 5;
+let num2 = 10;
+
+// Exercise 6. Write a comment below explaining the difference between the rules
+// for num1 and num2 from Exercise 5. What numbers would be valid values for
+// num2 but not num1?
+// num1 must be >1 and <10
+// num2 can include 1 and 10
+// So 1 and 10 are valid for num2 but not num1.
+
+// Exercise 7. Now we will try some addition.
+num1 = 4;
+num2 = 6;
+let theSum = num1 + num2;
+
+// Exercise 8. Now we will try some multiplication.
+let theProduct = num1 * num2;
+
+console.log("Sum:", theSum);
+console.log("Product:", theProduct);
